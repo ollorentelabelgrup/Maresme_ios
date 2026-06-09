dd03cb7309718aceb3328617fbacf9895649c47a
macbookpro <info@fomplay.com>
2026-06-09 12:32:29 +0200

    Initial Commit

---
+++ Maresme/MaresmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaresmeApp.swift
+//  Maresme
+//
+//  Created by Oscar Llorente Serrano on 09/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MaresmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
